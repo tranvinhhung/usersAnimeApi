7d1801a7d1c859238d0c75e2fc30afc717a28fa0
tranvinhhung <tranvinhung2k@gmail.com>
2022-02-02 14:25:06 +0700

    1cm

---
+++ routes/tourRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+// router.param('id', tourController.checkID);
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+router
+  .route('/')
+  .get(authController.protect, tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
+
+module.exports = router;
